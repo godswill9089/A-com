9cc5670393cb4b08da012f2570070c85e1190ae7
godswill9089 <ejimoforgodswill60@gmail.comm>
2024-03-23 20:21:25 +0100

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style';
-import { discount, robot } from '../assets';
+import { discount } from '../assets';
+import rob from "../assets/rob.png";
 import GetStarted from './GetStarted';
 import "./Hero.css";
 
@@ -8,7 +9,7 @@ const Hero = () => {
   return (
    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+        <div className='flex flex-row items-center py-[6px] px-4 hero bg-discount-gradient rounded-[10px] mb-2'>
           <img src={discount} alt='discount' className='w-[32px] h-[32px]'/>
           <p className={`${styles.paragraph} ml-2`}>
           <span className='text-white'>We HELP You</span> Manifest Your {" "}
@@ -37,8 +38,8 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-10 my-10 relative`}>
-        <img src={robot} alt='billing' className='w-[100%]  relative z-[5] floating-image'/>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-10 my-10 image-dev relative`}>
+        <img src={rob} alt='billing' className='w-[100%]  relative z-[5] floating-image'/>
 
         <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
         <div className='absolute z-[1] w-[40%] h-[80%] rounded-full bottom-40 white__gradient'></div>
